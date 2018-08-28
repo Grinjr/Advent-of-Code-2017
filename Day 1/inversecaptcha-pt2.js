8eb2ada6775fd5ed17f8ff15fe51055fffbe178f
Grinjr <Grinjr@yahoo.com>
2018-08-28 16:17:26 -0700

    Update inversecaptcha-pt2.js
    replaced all the mentions of result.input.length with just len to make things shorter and more readable

--- Day 1/inversecaptcha-pt2.js
+++ Day 1/inversecaptcha-pt2.js
@@ -3,8 +3,8 @@ var prompt = require('prompt');
 var output = 0;
 prompt.start();
 prompt.get('input', function(err, result) {
-	for (var i = 0; i < result.input.length; i++) {
-		var nexti = i + (result.input.length/2) - (Math.floor((1/result.input.length)*(i+(result.input.length/2)))*result.input.length);
+	for (var i = 0, len = result.input.length; i < len; i++) {
+		var nexti = i + (len/2) - (Math.floor((1/len)*(i+(len/2)))*len);
 		if (result.input.charAt(i) == result.input.charAt(nexti)) {
 			output += parseInt(result.input.charAt(i));
 		}
